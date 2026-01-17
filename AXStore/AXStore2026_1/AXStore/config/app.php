1c88f57c5e023c11abd7f225a2c4720525ed7b2d
SkayFake <steven19denoviembre@gmail.com>
2026-01-16 21:58:19 -0600

    Proyecto subido correctamente con archivos

---
+++ AXStore/AXStore2026_1/AXStore/config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+	const APP_URL="http://localhost:8080/AXStore/AXStore2026_1/AXStore/";
+	const APP_NAME="AXStore";
+	const APP_SESSION_NAME="AXStore";
+
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/El_Salvador");
---
+++ AXStore2026_1/config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+	/*----------  Configuración General Josue ----------*/
+	const APP_URL="http://localhost:8080/AXStore2026_1/";
+	const APP_NAME="AXStore";
+	const APP_SESSION_NAME="AXStore";
+
+	/*----------  Configuración General Descomentar ----------*/
+	//const APP_URL="http://localhost/AXStore2026_1/";
+	//const APP_NAME="AXStore";
+	//const APP_SESSION_NAME="AXStore";
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/El_Salvador");
