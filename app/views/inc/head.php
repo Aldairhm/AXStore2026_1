b5bd9429185aa95e53d4c9ff6793414c681045d0
SkayFake <steven19denoviembre@gmail.com>
2026-02-13 17:42:04 -0600

    Limpio header

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -12,4 +12,5 @@
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/assets/css/style.css?v=77">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/assets/css/card.css?v=23">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/assets/css/enviosHome.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/assets/css/headerstyle.css?v=12">
 </head>
